8567aa2de3acc5804000bf55c60e7ed0949b6a79
slimane rechdi <slimane.rechdi@gmail.com>
2020-08-13 11:38:28 -0400

    repackaged

--- compile.bat
+++ compile.bat
@@ -1,7 +1,11 @@
 :: Set this file for compiling the executable.
 :: So it can be added to the user custom theme in solidedge. 
-ipyc.exe /main:./bullet/__main__.py ./bullet/api.py ./bullet/holes.py ./bullet/equivalences.py ./bullet/customs.py ^
-./bullet/Interop.SolidEdge.dll ^
+ipyc.exe /main:__main__.ipy 
+api.ipy ^
+holes.ipy ^
+equivalences.ipy ^ 
+customs.ipy ^
+Interop.SolidEdge.dll ^
 /embed ^
 /out:bullet_macro-recs_64x_0-0-9 ^
 /platform:x64 ^
